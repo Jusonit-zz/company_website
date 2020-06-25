c77b1745da0a795175a864b77e4958c06327cd5f
Junior Figuereo <juniorsj@live.com>
2020-06-25 15:54:55 -0400

    fixed adding service to list

--- Pear_Store/js/customers.js
+++ Pear_Store/js/customers.js
@@ -2,9 +2,15 @@ function SubmitData() {
     var name = document.getElementById("name").value;
     var username = document.getElementById("username").value;
     var email = document.getElementById("email").value;
+    var mailing = "dummy value";
+    
+    var services=[];
+    $("input:checkbox[name=service]:checked").each(function(){
+        services.push($(this).val());
+    });
 
     var rows = "";
-    rows += "<td>" + name + "</td><td>" + username + "</td><td>" + email + "</td>";
+    rows += "<td>" + name + "</td><td>" + username + "</td><td>" + email + "</td><td>" + mailing + "</td><td>" + services.join(', ') + "</td>";
     var tbody = document.querySelector("#list tbody");
     var tr = document.createElement("tr");
 
