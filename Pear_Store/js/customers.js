ff849d07647ed48d5b6f3be70c09186a4d8662d4
Junior S Figuereo <juniorsj@live.com>
2020-06-25 15:09:19 -0400

    Update customers.js

--- Pear_Store/js/customers.js
+++ Pear_Store/js/customers.js
@@ -1,7 +1,7 @@
 function SubmitData() {
-    var name = document.getElementById("name").value;
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
+    var name = document.getElementById("inputName").value;
+    var username = document.getElementById("inputUsername").value;
+    var email = document.getElementById("inputEmail").value;
     
     var rows = "";
     rows += "<td>" + name + "</td><td>" + username + "</td><td>" + email + "</td>";
@@ -11,4 +11,5 @@ function SubmitData() {
     tr.innerHTML = rows;
     tbody.appendChild(tr);
 }
+
 
