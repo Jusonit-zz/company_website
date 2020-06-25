56a130764fcdb351d675cdf7697e54a2e8ea42fe
ludiusvox <ludiusvox@gmail.com>
2020-06-25 12:31:36 -0400

    Update products_and_services.js

--- Pear_Store/products_and_services.js
+++ Pear_Store/products_and_services.js
@@ -1,4 +1,4 @@
-function search_animal() { 
+function search_products() { 
     let input = document.getElementById('searchbar').value 
     input=input.toLowerCase(); 
     let x = document.getElementsByClassName('products'); 
